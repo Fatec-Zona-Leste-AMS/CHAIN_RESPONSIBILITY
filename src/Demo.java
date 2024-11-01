6b03368408fd48d46669c16ada952da77eacb09d
Jefferson Oliveira <jefferoidtex7@gmail.com>
2024-11-01 20:31:01 -0300

    feat: create a projet

---
+++ src/Demo.java
@@ -0,0 +1,54 @@
+import middleware.Middleware;
+import middleware.RoleCheckMiddleware;
+import middleware.ThrottlingMiddleware;
+import middleware.UserExistsMiddleware;
+import server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+
+        // All checks are linked. Client can build various chains using the same components.
+        Middleware middleware = Middleware.link(
+                new ThrottlingMiddleware(2),
+                new UserExistsMiddleware(server),
+                new RoleCheckMiddleware()
+        );
+
+        // Server gets a chain from client code.
+        server.setMiddleware(middleware);
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Input password: ");
+            String password = reader.readLine();
+
+            try {
+                success = server.logIn(email, password);
+            } catch (RuntimeException e) {
+                // Catching the exception from ThrottlingMiddleware and providing a friendly message.
+                System.out.println(e.getMessage());
+                System.out.println("Please wait a while before trying again.");
+                success = false; // Keep looping to allow the user to retry
+            }
+        } while (!success);
+    }
+}
